7787a8640ac28b69533c73aa429f855c4a2f2308
Ankush Thakur <atpara99@gmail.com>
2022-06-23 12:26:31 +0530

    Project Ready

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Forget from "./Components/Forget";
+import Login from "./Components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      {/* <Forget /> */}
+    </div>
+  );
+}
+
+export default App;
